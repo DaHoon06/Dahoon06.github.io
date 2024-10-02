7ad1297953366e9146c7da8bca7c1945d85936c1
dahoon06 <dahoon226@naver.com>
2024-10-02 14:11:46 +0900

    init

---
+++ src/layouts/BaseLayout.tsx
@@ -0,0 +1,59 @@
+import { StaticHeader, MobileHeader } from "./header";
+import { Footer } from "./Footer";
+import { ReactNode, useEffect, useState } from "react";
+import styled from "styled-components";
+
+interface BaseLayoutProps {
+  children: ReactNode;
+}
+
+const BaseContainer = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+const Main = styled.main<{$isScrolled: boolean}>`
+  width: 100%;
+  height: 100%;
+  min-height: calc(100vh - 236px);
+  padding: 0 1em;
+  background-color: ${({ theme }) => theme.colors.background.base};
+  margin-top: ${({$isScrolled}) => $isScrolled ? 60 : 100}px;
+`;
+
+const MainContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  margin: auto;
+  padding: 1em;
+`;
+
+export const BaseLayout = ({ children }: BaseLayoutProps) => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  const handleScroll = ({target}: any) => {
+    if (target.scrollTop > 200) {
+      setIsScrolled(true);
+    } else {
+      setIsScrolled(false);
+    }
+  };
+
+  useEffect(() => {
+    handleScroll({target: document.body});
+    document.body.addEventListener('scroll', handleScroll);
+    return () => document.body.removeEventListener('scroll', handleScroll)
+  }, []);
+
+  return (
+    <>
+      <BaseContainer>
+        {isScrolled ? <MobileHeader isShow={isScrolled} /> : <StaticHeader isShow={isScrolled} />}
+        <Main $isScrolled={isScrolled}>
+          <MainContainer>{children}</MainContainer>
+        </Main>
+      </BaseContainer>
+      <Footer />
+    </>
+  );
+};
---
+++ src/styles/styled-components/index.ts
@@ -0,0 +1,5 @@
+import { GlobalStyle } from "./global-style";
+import { media, scroll } from "./theme";
+import { colors, light, dark } from "@styles/styled-components/pallet";
+
+export { GlobalStyle, media, scroll, colors, light, dark };
