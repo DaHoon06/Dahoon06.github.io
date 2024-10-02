dc82e5043645fab1b6693aba68cdc980aa8631d9
dahoon06 <dahoon226@naver.com>
2024-10-02 13:26:09 +0900

    init

--- application/next.config.mjs
+++ application/next.config.mjs
@@ -1,7 +1,13 @@
+const assetPrefix =
+  process.env.NODE_ENV === "production"
+    ? "https://dahoon06.github.io"
+    : "";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
+  assetPrefix
 };
 
 export default nextConfig;
